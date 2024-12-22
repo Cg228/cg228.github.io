bd78ffd8c107235624a8a6902100ae509b701f4c
cg228 <camg228@outlook.com>
2024-12-22 05:42:13 -0500

    num 5

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // script.js
 
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const buffer = 150; 
+
+    return (
+        rect.top + buffer <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left >= 0 &&
+        rect.bottom - buffer >= 0 &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
 // Add class to items when they come into view
 function checkTimelineVisibility() {
     const timelineItems = document.querySelectorAll('.timeline-item');
